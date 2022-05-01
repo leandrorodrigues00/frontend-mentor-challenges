b9180b756f171a6096ad0830a05bbccf61f93fc9
Leandro Rodrigues <leandro.rodrigues00@hotmail.com>
2022-05-01 20:43:09 -0300

    ♻️  refactor: refatorando interactive rating component

--- interactive-rating-component-main/interactive-rating-component-main/script.js
+++ interactive-rating-component-main/interactive-rating-component-main/script.js
@@ -4,9 +4,17 @@ const secundario = document.querySelector(".thanks");
 const buttonMenu = document.querySelectorAll("[data-value]");
 
 const numberStar = document.querySelector(".number-star-seleced");
+
+let selectedRating = null;
+
 btn.addEventListener("click", handleChange);
 
 function handleChange() {
+  selectedRating = value;
+  if (!selectedRating) {
+    return alert("Please select number rating!");
+  }
+
   principal.classList.toggle("ativo");
   secundario.classList.toggle("ativo");
 }
