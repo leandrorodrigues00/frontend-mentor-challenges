293a09c76b991c9a364dc1c1ec10fbefe29002c0
Leandro Rodrigues <leandro.rodrigues00@hotmail.com>
2022-10-13 22:28:21 -0300

    ✨ feat: Product preview done!

---
+++ product-preview/src/App.jsx
@@ -0,0 +1,42 @@
+import "./global.css";
+import styles from "./App.module.css";
+
+import desktop from "./assets/images/image-product-desktop.jpg";
+import mobile from "./assets/images/image-product-mobile.jpg";
+
+import carIcon from "./assets/images/icon-cart.svg";
+function App() {
+  return (
+    <main className={styles.mainWrap}>
+      <div>
+        <picture>
+          <source srcSet={mobile} media="(max-width: 625px)" />
+          <img className={styles.img} src={desktop} type="image/jpg" />
+        </picture>
+      </div>
+
+      <div className={styles.content}>
+        <header className={styles.headerContent}>Perfume</header>
+        <h1 className={styles.titleContent}>
+          <a href="#"> Gabrielle Essence Eau De Parfum</a>
+        </h1>
+        <p className={styles.descriptionContent}>
+          A floral, solar and voluptuous interpretation composed by Olivier
+          Polge, Perfumer-Creator for the House of CHANEL.
+        </p>
+
+        <div className={styles.priceContent}>
+          <span className={styles.price}>$149.99</span>
+          <span className={styles.oldPrice}>$169.99</span>
+        </div>
+
+        <button className={styles.button}>
+          <img className={styles.icon} src={carIcon} />
+          Add to cart
+        </button>
+      </div>
+    </main>
+  );
+}
+
+export default App;
