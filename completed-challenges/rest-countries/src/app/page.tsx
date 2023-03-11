f0dd140d6a36f2abb2900c4ff11de82ae97db579
Leandro Rodrigues <leandro.rodrigues00@hotmail.com>
2023-03-11 14:02:57 -0300

    fix: remove carflags - route handlers

--- completed-challenges/rest-countries/src/app/page.tsx
+++ completed-challenges/rest-countries/src/app/page.tsx
@@ -27,89 +27,77 @@ async function getCountriesData(
   apiUrl: string,
   selectedRegion: string | boolean,
 ) {
-  try {
-    const response = await fetch(apiUrl, {
-      cache: 'no-store',
-    })
+  const response = await fetch(apiUrl, {
+    cache: 'no-store',
+  })
 
-    if (!response.ok) {
-      console.log(response)
-      return []
-    }
-
-    const responseData: unknown = await response.json()
-
-    const countriesData: Array<CountriesProps> = Array.isArray(responseData)
-      ? responseData
-      : []
-
-    countriesData.sort((a, b) =>
-      ('' + a.name.common).localeCompare(b.name.common),
-    )
-
-    const countries = countriesData
-      .filter(
-        (data: CountriesProps) =>
-          !selectedRegion || data.region === selectedRegion,
-      )
-      .map(({ name, flags, capital, region, population, cca3 }) => ({
-        name,
-        flags,
-        capital,
-        region,
-        population,
-        cca3,
-      }))
-
-    return countries
-  } catch (error) {
-    console.error(error)
+  if (!response.ok) {
     return []
   }
+
+  const responseData: unknown = await response.json()
+
+  const countriesData: Array<CountriesProps> = Array.isArray(responseData)
+    ? responseData
+    : []
+
+  countriesData.sort((a, b) =>
+    ('' + a.name.common).localeCompare(b.name.common),
+  )
+
+  const countries = countriesData
+    .filter(
+      (data: CountriesProps) =>
+        !selectedRegion || data.region === selectedRegion,
+    )
+    .map(({ name, flags, capital, region, population, cca3 }) => ({
+      name,
+      flags,
+      capital,
+      region,
+      population,
+      cca3,
+    }))
+
+  return countries
 }
 
 export default async function Home() {
-  try {
-    const responseApiCountries = await fetch(
-      `${process.env.NEXT_PUBLIC_URL}/api/countries`,
-      {
-        cache: 'no-store',
-      },
-    )
+  const responseApiCountries = await fetch(
+    `${process.env.NEXT_PUBLIC_URL}/api/countries`,
+    {
+      cache: 'no-store',
+    },
+  )
+  const { source, region }: DataAPI = await responseApiCountries.json()
 
-    const { source, region }: DataAPI = await responseApiCountries.json()
+  const selectedRegion: string | boolean =
+    source === 'radixSelect' ? region : false
 
-    const selectedRegion: string | boolean =
-      source === 'radixSelect' ? region : false
+  const InputSearch: string | boolean =
+    source === 'InputSearch' ? region : false
 
-    const InputSearch: string | boolean =
-      source === 'InputSearch' ? region : false
+  const apiUrl = InputSearch
+    ? `https://restcountries.com/v3.1/name/${InputSearch}`
+    : selectedRegion
+    ? `https://restcountries.com/v3.1/region/${selectedRegion}?fields=name,capital,region,population,flags,cca3`
+    : `https://restcountries.com/v3.1/all?fields=name,capital,region,population,flags,cca3`
 
-    const apiUrl = InputSearch
-      ? `https://restcountries.com/v3.1/name/${InputSearch}`
-      : selectedRegion
-      ? `https://restcountries.com/v3.1/region/${selectedRegion}?fields=name,capital,region,population,flags,cca3`
-      : `https://restcountries.com/v3.1/all?fields=name,capital,region,population,flags,cca3`
+  const countries = await getCountriesData(apiUrl, selectedRegion)
 
-    const countries = await getCountriesData(apiUrl, selectedRegion)
-
-    return (
-      <main className="px-[4.68rem] pt-10 max-[580px]:px-3">
-        <header className="flex justify-between gap-3 max-[600px]:flex-col max-[600px]:gap-8">
-          <InputSearchCountryHome />
-          <SelectRegions />
-        </header>
-        {apiUrl}
-        <div className="mt-11 grid grid-cols-1 gap-y-12 min-[680px]:grid-cols-2 min-[940px]:grid-cols-3 min-[1440px]:grid-cols-5 justify-items-center mb-10">
-          {countries?.map((country) => (
-            <Link href={`/country/${country.cca3}`} key={country.name.common}>
-              <CardFlags country={country} />
-            </Link>
-          ))}
-        </div>
-      </main>
-    )
-  } catch (error) {
-    console.error(error)
-  }
+  return (
+    <main className="px-[4.68rem] pt-10 max-[580px]:px-3">
+      <header className="flex justify-between gap-3 max-[600px]:flex-col max-[600px]:gap-8">
+        <InputSearchCountryHome />
+        <SelectRegions />
+      </header>
+      {/* <div className="mt-11 grid grid-cols-1 gap-y-12 min-[680px]:grid-cols-2 min-[940px]:grid-cols-3 min-[1440px]:grid-cols-5 justify-items-center mb-10">
+        {countries?.map((country) => (
+          <Link href={`/country/${country.cca3}`} key={country.name.common}>
+            <CardFlags country={country} />
+          </Link>
+        ))}
+      </div> */}
+    </main>
+  )
 }
